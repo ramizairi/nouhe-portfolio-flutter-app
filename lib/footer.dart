b6040892240284126e37385081df2c57637899d7
ramizairi <ramizeyri@gmail.com>
2025-01-11 01:28:50 +0100

    first commit

---
+++ lib/footer.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:portfolio_x/header.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'colors.dart';
+
+class FooterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return VStack(
+      [
+        VxDevice(
+          mobile: VStack(
+            [
+              "Got a project?\nLet's talk.".text.center.white.xl2.make(),
+              10.heightBox,
+              "mtechviral@gmail.com"
+                  .text
+                  .color(Coolors.accentColor)
+                  .semiBold
+                  .make()
+                  .box
+                  .border(color: Coolors.accentColor)
+                  .p16
+                  .rounded
+                  .make()
+            ],
+            crossAlignment: CrossAxisAlignment.center,
+          ),
+          web: HStack(
+            [
+              "Got a project?\nLet's talk.".text.center.white.xl2.make(),
+              10.widthBox,
+              "mtechviral@gmail.com"
+                  .text
+                  .color(Coolors.accentColor)
+                  .semiBold
+                  .make()
+                  .box
+                  .border(color: Coolors.accentColor)
+                  .p16
+                  .rounded
+                  .make(),
+            ],
+            alignment: MainAxisAlignment.spaceAround,
+          ).w(context.safePercentWidth * 70).scale150().p16(),
+        ),
+        50.heightBox,
+        CustomAppBar(),
+        10.heightBox,
+        "Thanks for scrolling, ".richText.semiBold.white.withTextSpanChildren(
+            ["that's all folks.".textSpan.gray500.make()]).make(),
+        10.heightBox,
+        SocialAccounts(),
+        30.heightBox,
+        [
+          "Made With 🤍 By Nouhe".text.red500.make(),
+          10.widthBox,
+        ].hStack(crossAlignment: CrossAxisAlignment.center)
+      ],
+      crossAlignment: CrossAxisAlignment.center,
+    ).wFull(context).p16();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio_x/colors.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PortfolioX',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: Coolors.secondaryColor),
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
